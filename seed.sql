38344cea6e615638f2fd51ea09fc319b344a6709
Greg Roberts <groberts3104@gmail.com>
2020-01-17 22:08:19 -0500

    added emplyee table

--- seed.sql
+++ seed.sql
@@ -17,3 +17,10 @@ CREATE TABLE role(
     department_id INT NOT NULL
 );
 
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+)
