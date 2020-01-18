59a93c7522216e3c8cc3fef071336236475c7d31
Greg Roberts <groberts3104@gmail.com>
2020-01-17 22:06:29 -0500

    added role table with columns

--- seed.sql
+++ seed.sql
@@ -8,4 +8,12 @@ CREATE TABLE department
 (
     id INTEGER PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-)
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL
+);
+
