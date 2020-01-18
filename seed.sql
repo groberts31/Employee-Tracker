7177aa19e9009b39012e8ebf448c7f5060f00d3f
Greg Roberts <groberts3104@gmail.com>
2020-01-17 22:02:38 -0500

    added table for department

--- seed.sql
+++ seed.sql
@@ -3,3 +3,10 @@ DROP DATABASE IF EXISTS emp_tracker_db;
 CREATE DATABASE emp_tracker_db;
 
 USE emp_tracker_db;
+
+CREATE TABLE department
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+)
